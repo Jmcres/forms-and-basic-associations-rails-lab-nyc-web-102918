8bf7d4f9e3f26e4f35ab651a8a5b3faabf6e7f56
github username <github email address>
2018-11-27 11:15:59 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,12 +3,12 @@ class ArtistsController < ApplicationController
     @artists = Artist.all
   end
 
-  def show
-    @artist = Artist.find(params[:id])
-  end
-
   def new
     @artist = Artist.new
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
   end
 
   def create
